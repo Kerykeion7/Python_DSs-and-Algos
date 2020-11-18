eccb4b5353c6b5f841c8ebb728eb54bdfbde46b6
Jens D <jensdeweirdt@outlook.be>
2020-11-18 16:16:11 +0100

    Begin ArrayQ Unit Tests

--- Python_DSs-and-Algos.proj/Queues/ArrayQueues/test_ArrayQ.py
+++ Python_DSs-and-Algos.proj/Queues/ArrayQueues/test_ArrayQ.py
@@ -2,9 +2,21 @@ from Queues.ArrayQueues import ArrayQ as Qclass
 import unittest
 
 class Test_test_ArrayQ(unittest.TestCase):
-    def test_A(self):
+    def __setUpMainArrayQ(self):
+        q = Qclass.ArrayQ()
+        q.enqueue(1)
+        q.enqueue(3)
+        q.enqueue(5)
+        q.enqueue(6)
+        return q
+
+    def test_Q_Not_None(self):
         q = Qclass.ArrayQ()
         self.assertFalse(q is None)
+
+    def test_Q_Count_Equals_Four(self):
+        q = self.__setUpMainArrayQ()
+        self.assertEqual(4, q.getSize())
 
 if __name__ == '__main__':
     unittest.main()
